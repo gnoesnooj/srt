d3e9855a1a70808fe37eb6d924692e572292e7ca
gnoesnooj <306yyy@naver.com>
2022-08-23 20:52:41 +0800

    feat : 단축 url request dto 작성

---
+++ src/main/java/com/srt/srt/dto/EncodeRequestDto.java
@@ -0,0 +1,13 @@
+package com.srt.srt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EncodeRequestDto {
+
+    private String originUrl;
+}
